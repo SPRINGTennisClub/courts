6514da4f3ea8cd8d300debaa40ec6459ad4543a3
aubertMal <malek.aubert@gmail.com>
2021-07-22 10:33:31 +0200

    feat: create courts service

---
+++ src/main/java/com/aubert/courts/CourtsApplication.java
@@ -0,0 +1,13 @@
+package com.aubert.courts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aubert/courts/CourtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aubert.courts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
